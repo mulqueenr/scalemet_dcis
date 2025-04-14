18ba3567ee5dfae025d2333b5c1739aeafb75c0a
Ryan Mulqueen <mulqueenr@gmail.com>
2025-04-14 14:27:50 -0500

    Update scaleDCIS_postprocessing.nf.groovy

--- src/scaleDCIS_postprocessing.nf.groovy
+++ src/scaleDCIS_postprocessing.nf.groovy
@@ -133,11 +133,10 @@ workflow {
 
     COUNT_READS(indir) 
 
-    cell_pf = COUNT_READS.out.cells_pf
-    .splitCsv(sep="\t", header: false)
-    .map { row -> tuple(row[0], row[1], row[2]) }
-    
-    SPLIT_BAMS(cell_pf) \
+    COUNT_READS.out.cells_pf \
+    | splitCsv(sep="\t", header: false) \
+    | map { row -> tuple(row[0], row[1], row[2]) } \
+    | SPLIT_BAMS \
     | collect \
     | COPYKIT
 
