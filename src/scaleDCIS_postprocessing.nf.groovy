4579c69b04787d66171fd4a84ad636e7b227a997
Ryan Mulqueen <mulqueenr@gmail.com>
2025-04-24 13:16:32 -0500

    update maxforks

--- src/scaleDCIS_postprocessing.nf.groovy
+++ src/scaleDCIS_postprocessing.nf.groovy
@@ -58,7 +58,7 @@ process COUNT_READS {
 process SPLIT_BAMS {
 	//Take output of COUNT_READS with each line being a tuple, then split from source bam to single cell bam
     publishDir "${params.runDir}/cnv/sc_bam", mode: 'copy', overwrite:true, pattern: "*bam"
-	forks "${params.max_forks}"
+	forks "${params.max_cpus}"
 	input:
 		tuple val(read_count),val(idx),path(bam)
 	output:
