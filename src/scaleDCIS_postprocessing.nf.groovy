4454a81506fdc8e9e4adf614fa2564ab75cd555d
Ryan Mulqueen <mulqueenr@gmail.com>
2025-04-24 13:28:09 -0500

    Update scaleDCIS_postprocessing.nf.groovy

--- src/scaleDCIS_postprocessing.nf.groovy
+++ src/scaleDCIS_postprocessing.nf.groovy
@@ -58,7 +58,7 @@ process COUNT_READS {
 process SPLIT_BAMS {
 	//Take output of COUNT_READS with each line being a tuple, then split from source bam to single cell bam
     publishDir "${params.runDir}/cnv/sc_bam", mode: 'copy', overwrite:true, pattern: "*bam"
-	maxForks ${params.maxForks}
+	maxForks "${params.maxForks}"
 
 	input:
 		tuple val(read_count),val(idx),path(bam)
